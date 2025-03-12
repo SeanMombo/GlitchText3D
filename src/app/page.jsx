cafc934fa64a6464cd4e2fb8738e14210d3243c4
seanmombo <sean@mombourquette.com>
2025-03-12 15:02:15 -0400

    setup canvas overlay and enabled scrolling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import R3fCanvas from "@/components/canvas";
 
+
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
+    <div className="font-[family-name:var(--font-geist-sans)] relative overflow-x-hidden pointer-events-auto">
       <R3fCanvas/>
-       
+      <div style={{ height: '100vh', width: '100vw', marginTop: '200vh' }}></div>
+
     </div>
   );
 }
--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -1,25 +1,37 @@
 "use client"
 import { Canvas } from '@react-three/fiber'; // Import Canvas from R3F
-import { OrbitControls } from '@react-three/drei'; // Optional: Adds camera controls
-
+import { Center, OrbitControls } from '@react-three/drei'; // Optional: Adds camera controls
+import { Text3D } from '@react-three/drei';
+import customFont from './Nau_Sea_Regular.json'
+import { Text } from '@react-three/drei';
+import { useEffect, useState, useRef } from 'react';
 
 const R3fCanvas = () => {
+    const scrollY = useRef();
+    useEffect(() => {
+
+        const onScroll = () => {
+            scrollY.current = window.scrollY;
+        };
+
+        window.addEventListener('scroll', onScroll)
+        return () => {
+            window.removeEventListener('scroll', onScroll);
+        }
+    },[])
+
     return (
-      <div style={{ height: '100vh', width: '100vw' }}>
+      <div style={{ height: '100vh', width: '100vw', position: 'fixed', top: 0, pointerEvents: 'none' }}>
         {/* Canvas element where the 3D scene will be rendered */}
-        <Canvas>
+        <Canvas >
           {/* Lights */}
           <ambientLight intensity={0.5} /> {/* Ambient light */}
           <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} /> {/* Spot light */}
   
           {/* 3D Object */}
-          <mesh position={[-1.2, 0, 0]}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color="orange" />
-          </mesh>
-  
+          <Text color="white">How are you feeling?</Text>  
+
           {/* OrbitControls for interactive camera */}
-          <OrbitControls />
         </Canvas>
       </div>
     );
